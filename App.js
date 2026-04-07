f2bb60dc01e9f0b30c8eeee3a33a609ab759e2fd
daniella silva <danizinha.40.silva@gmail.com>
2026-04-07 15:57:43 -0400

    Commit atualização

---
+++ App.js
@@ -0,0 +1,18 @@
+const r = require('readline-sync')
+
+// média com while 
+
+let somaDoWhile = 0;
+let contadorDoWhile = 1;
+
+do {
+    let nota = parseFloat(r.question(`Nota ${contadorDoWhile}: `));
+    somaDoWhile += nota;
+    contadorDoWhile++;
+
+} while (contadorDoWhile <= 5);
+
+let mediaDoWhile = somaDoWhile / 5;
+console.log("A média é: " + mediaDoWhile.toFixed(2));
+        
+    
